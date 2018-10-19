4b15bdc12871f891cbaff092933444e7f49dac7d
caue rego <im@cregox.net>
2018-10-19 13:23:54 +0000

    remove semi from random js

--- random/main.js
+++ random/main.js
@@ -6,53 +6,55 @@
 *  tree.
 */
 
+/*eslint-disable semi */
 'use strict';
 
-const videoElement = document.querySelector('video');
-const audioInputSelect = document.querySelector('select#audioSource');
-const videoSelect = document.querySelector('select#videoSource');
-const selectors = [audioInputSelect, videoSelect];
+const videoElement = document.querySelector('video')
+const audioInputSelect = document.querySelector('select#audioSource')
+const videoSelect = document.querySelector('select#videoSource')
+const selectors = [audioInputSelect, videoSelect]
 
 
 function gotDevices(deviceInfos) {
   // Handles being called several times to update labels. Preserve values.
-  const values = selectors.map(select => select.value);
+  const values = selectors.map(select => select.value)
   selectors.forEach(select => {
     while (select.firstChild) {
-      select.removeChild(select.firstChild);
+      select.removeChild(select.firstChild)
     }
-  });
+  })
   for (let i = 0; i !== deviceInfos.length; ++i) {
-    const deviceInfo = deviceInfos[i];
-    const option = document.createElement('option');
-    option.value = deviceInfo.deviceId;
+    const deviceInfo = deviceInfos[i]
+    const option = document.createElement('option')
+    option.value = deviceInfo.deviceId
     if (deviceInfo.kind === 'audioinput') {
-      option.text = deviceInfo.label || `microphone ${audioInputSelect.length + 1}`;
-      audioInputSelect.appendChild(option);
+      option.text = deviceInfo.label || `microphone ${audioInputSelect.length + 1}`
+      audioInputSelect.appendChild(option)
     } else if (deviceInfo.kind === 'videoinput') {
-      option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`;
-      videoSelect.appendChild(option);
+      option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`
+      videoSelect.appendChild(option)
     } else {
-      console.log('Some other kind of source/device: ', deviceInfo);
+      console.log('Some other kind of source/device: ', deviceInfo)
     }
   }
   selectors.forEach((select, selectorIndex) => {
     if (Array.prototype.slice.call(select.childNodes).some(n => n.value === values[selectorIndex])) {
-      select.value = values[selectorIndex];
+      select.value = values[selectorIndex]
     }
-  });
+  })
 }
 
-navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
+navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError)
 
 function gotStream(stream) {
   window.stream = stream // make stream available to console
-  videoElement.srcObject = stream
+//  videoElement.srcObject = stream
   // Refresh button list in case labels have become available
   return navigator.mediaDevices.enumerateDevices()
 }
 
 function handleError(error) {
+  
   console.log('navigator.getUserMedia error: ', error)
 }
 
